bfe62b34271029710a18aebc0e7d925941b253ea
sandoschwert <sandi.schwert@gmail.com>
2017-03-23 23:16:35 -0500

    fixing error and rerunning bundle install and update

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -30,7 +30,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "shoud get contact" do
+  test "should get contact" do
     get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
