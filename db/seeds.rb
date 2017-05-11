b386fd305e28f170a51b7734966e85e5e7e05ae5
sandoschwert <sandi.schwert@gmail.com>
2017-05-11 11:48:46 -0500

    adding mid chapter 13

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+User.create!(name:  "Sando",
+             email: "sandoschwert@gmail.com",
+             password:              "abc123",
+             password_confirmation: "abc123",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -16,4 +24,10 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
